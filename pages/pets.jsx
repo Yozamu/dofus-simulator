ad4b78cb473fe00149ed9610cd6c4fd3ef0a578e
Yozamu <yozamu@hotmail.fr>
2022-12-30 12:11:36 +0100

    Pets page

---
+++ pages/pets.jsx
@@ -0,0 +1,32 @@
+import { retrieveItems } from './api/items';
+import ItemsPage from '../components/items/ItemsPage';
+import { PETS, WEAPONS } from '../helpers/constants';
+
+const PetsPage = ({ items, query }) => {
+  const availableCategories = ['Dragodinde', 'Muldo', 'Familier', 'Montilier'];
+
+  return (
+    <ItemsPage
+      title="Familiers & Montures"
+      query={query}
+      items={items}
+      availableCategories={availableCategories}
+      itemHeight={300}
+    />
+  );
+};
+
+export async function getServerSideProps(context) {
+  const query = { ...context.query, type: PETS };
+  const req = { ...context.req, query };
+  const res = await retrieveItems(req);
+  return {
+    props: {
+      items: res.data,
+      count: res.count,
+      query,
+    },
+  };
+}
+
+export default PetsPage;
