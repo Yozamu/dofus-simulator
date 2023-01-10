72f8b68bda9cdb3765924c807b19fd550feb4ba0
Yozamu <yozamu@hotmail.fr>
2023-01-10 20:15:07 +0100

    revert broken file (fixed on vercel)

--- pages/equipment.jsx
+++ pages/equipment.jsx
@@ -1,17 +1,8 @@
-//import { retrieveItems } from './api/items';
+import { retrieveItems } from './api/items';
 import ItemsPage from '../components/items/ItemsPage';
-//import { EQUIPMENT } from '../helpers/constants';
-import { useEffect, useState } from 'react';
+import { EQUIPMENT } from '../helpers/constants';
 
-const EquipmentPage = (/*{ items, query }*/) => {
-  const [items, setItems] = useState([]);
-  const query = '';
-  useEffect(() => {
-    fetch('/api/items')
-      .then((res) => res.json())
-      .then((json) => setItems(json.data));
-  }, []);
-
+const EquipmentPage = ({ items, query }) => {
   const availableCategories = [
     'Amulette',
     'Anneau',
@@ -26,7 +17,7 @@ const EquipmentPage = (/*{ items, query }*/) => {
 
   return <ItemsPage title="Equipement" query={query} items={items} availableCategories={availableCategories} />;
 };
-/*
+
 export async function getServerSideProps(context) {
   const query = { ...context.query, type: EQUIPMENT };
   const req = { ...context.req, query };
@@ -39,5 +30,5 @@ export async function getServerSideProps(context) {
     },
   };
 }
-*/
+
 export default EquipmentPage;
